547957bf3048b46d6b8baa08b63bc704f35e7d37
mr-mizz <jmuli866@gmail.com>
2026-08-13 16:52:55 +0300

    Prepare application for production deployment

--- john-muli-portfolio/client/vite.config.js
+++ john-muli-portfolio/client/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  
-    base: '/john-muli-portfolio/',
+
+    base: '/Portfolio/',
 
   server: {
     port: 5173,
